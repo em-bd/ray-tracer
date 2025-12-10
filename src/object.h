010068abfd161071dc33edc0b3886c05c7f4fe09
Em Bradley-DeHaan <emmiesueb@gmail.com>
2025-12-10 16:42:35 -0600

    object structure created

--- src/object.h
+++ src/object.h
@@ -5,6 +5,17 @@
 
 #include "utils.h"
 
+/**
+ * Object data structure:
+ */
+typedef struct {
+    void* data;
+    hit_func hit;
+} object;
+
+/**
+ * Hit record data structure:
+ */
 typedef struct {
     point3 p;
     vec3 normal;
@@ -22,6 +33,8 @@ typedef struct {
     vec3 ta, tb, tc;
     vec3 n;
 } triangle;
+
+typedef bool (*hit_func)(object*, ray, interval, hit_record);
 
 bool hitSphere(ray, interval, hit_record*, sphere);
 
